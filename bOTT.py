6f55cb43ff420653488218735156188196221823
Kshitij A <kshitij.everest@gmail.com>
2021-10-17 11:42:09 +0530

    Changes to IMDb command

--- bOTT.py
+++ bOTT.py
@@ -62,6 +62,7 @@ async def imdb(ctx, *arg):
         rsoup = bs(rpage.text,'html.parser')
         rating = rsoup.find_all('span',class_='AggregateRatingButton__RatingScore-sc-1ll29m0-1 iTLWoV')[0].text
         embed=discord.Embed(title='IMDb Rating')
+        rating = rating+"/10"
         embed.add_field(name=title, value=rating, inline=False)
         embed.add_field(name="Visit the IMDb Page: -", value=f"[{title}]({hit})", inline=False)
         await ctx.send(embed=embed)
